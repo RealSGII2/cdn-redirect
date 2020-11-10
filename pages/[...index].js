36af8998f44f3ab9cc99752e7ea9c0f2c379898e
RealSGII2 <rbxsgii2@gmail.com>
2020-11-10 14:01:26 -0800

    yes

---
+++ pages/[...index].js
@@ -0,0 +1,17 @@
+import { useRouter } from 'next/router'
+
+export default function Home() {
+  return (
+    <p>Redirecting...</p>
+  )
+}
+
+export async function getServerSideProps(ctx) {
+  return {
+    props: {},
+    redirect: {
+      destination: `https://cdn.realsgii2.dev${ctx.resolvedUrl}`,
+      permanent: false,
+    },
+  }
+}
